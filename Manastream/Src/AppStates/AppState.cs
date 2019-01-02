4e0602929b8bd2946996cf89fe92dba5302af323
Tom Rushton <tom.rushton@hotmail.co.uk>
2019-01-02 12:47:17 +0000

    Create base appstate

---
+++ Manastream/Src/AppStates/AppState.cs
@@ -0,0 +1,28 @@
+﻿namespace Manastream.Src.AppStates
+{
+    #region Usings
+
+    using Microsoft.Xna.Framework.Graphics;
+
+    #endregion
+
+    /// <summary>
+    /// The appstate class.
+    /// </summary>
+    public abstract class AppState
+    {
+        #region Methods
+
+        /// <summary>
+        /// Draws the appstate.
+        /// </summary>
+        public void DrawState(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            
+            spriteBatch.End();
+        }
+
+        #endregion
+    }
+}
