4b39c59a761fbb72bec6047d3c70b1104a7aa65b
Tom Rushton <tom.rushton@hotmail.co.uk>
2019-01-05 23:21:05 +0000

    Create the ability class

---
+++ Manastream/Src/Gameplay/Abilities/Ability.cs
@@ -0,0 +1,56 @@
+﻿namespace Manastream.Src.Gameplay.Abilities
+{
+    #region Usings
+
+    using Manastream.Src.Gameplay.Abilities.Effects;
+    using Manastream.Src.Gameplay.Abilities.Templates;
+    using Manastream.Src.Gameplay.Enums;
+    using System.Collections.Generic;
+
+    #endregion
+
+    /// <summary>
+    /// The ability class.
+    /// </summary>
+    public class Ability
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="Ability"/> class.
+        /// </summary>
+        public Ability(string name, TargetType targetType, Template template, List<BaseEffect> effects)
+        {
+            this.Name = name;
+            this.TargetType = targetType;
+            this.Template = template;
+            this.Effects = effects;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public string Name
+        {
+            get;
+        }
+
+        public TargetType TargetType
+        {
+            get;
+        }
+
+        public Template Template
+        {
+            get;
+        }
+
+        public List<BaseEffect> Effects
+        {
+            get;
+        }
+
+        #endregion
+    }
+}
