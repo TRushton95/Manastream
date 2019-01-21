80c4119229dd1e5f467e60bd2a630a12506e8f7b
Tom Rushton <tom.rushton@hotmail.co.uk>
2019-01-21 17:16:59 +0000

    Add missing userAlertEvent

---
+++ Manastream/Src/EventSystem/Events/Debug/UserAlertEvent.cs
@@ -0,0 +1,22 @@
+﻿namespace Manastream.Src.EventSystem.Events.Debug
+{
+    /// <summary>
+    /// The user alert event.
+    /// <para>This event is for when pushing alerts to the user interface is the only
+    /// required behaviour - this is to reduce overhead in creating an event for every
+    /// scenario that will involve notifying the user only.</para>
+    /// </summary>
+    public class UserAlertEvent : Event
+    {
+        public UserAlertEvent(string message)
+            : base(EventTypes.Debug.UserAlert)
+        {
+            this.Message = message;
+        }
+
+        public string Message
+        {
+            get;
+        }
+    }
+}
