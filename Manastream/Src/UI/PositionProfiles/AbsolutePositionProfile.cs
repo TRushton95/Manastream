8450c1b9e5b0b9fc4591edc64f50dd49d85f31c2
Tom Rushton <tom.rushton@hotmail.co.uk>
2019-01-26 22:00:15 +0000

    Add missing comments

--- Manastream/Src/UI/PositionProfiles/AbsolutePositionProfile.cs
+++ Manastream/Src/UI/PositionProfiles/AbsolutePositionProfile.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.Xna.Framework;
-
-namespace Manastream.Src.UI.PositionProfiles
+﻿namespace Manastream.Src.UI.PositionProfiles
 {
+    #region Usings
+
+    using Microsoft.Xna.Framework;
+
+    #endregion
+
+    /// <summary>
+    /// The absolute position profile that defines a set of absolute coordinates.
+    /// </summary>
     public class AbsolutePositionProfile : IPositionProfile
     {
         #region Constructors
 
+        /// <summary>
+        /// Initialises a new instance of the <see cref="AbsolutePositionProfile"/> class.
+        /// </summary>
         public AbsolutePositionProfile(int posX, int posY)
         {
             this.PosX = posX;
@@ -19,19 +29,22 @@ namespace Manastream.Src.UI.PositionProfiles
         public int PosX
         {
             get;
-            private set;
+            set;
         }
 
         public int PosY
         {
             get;
-            private set;
+            set;
         }
 
         #endregion
 
         #region Methods
 
+        /// <summary>
+        /// Gets the position of the component.
+        /// </summary>
         public Vector2 GetPosition(Rectangle bounds, Rectangle parentBounds)
         {
             return new Vector2(PosX, PosY);
