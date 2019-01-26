e7acd3ebf2d3f486f3d65d3cdae4c0d9c61e2d2f
Tom Rushton <tom.rushton@hotmail.co.uk>
2019-01-26 21:54:41 +0000

    Add absolute position profile

---
+++ Manastream/Src/UI/PositionProfiles/AbsolutePositionProfile.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Manastream.Src.UI.PositionProfiles
+{
+    public class AbsolutePositionProfile : IPositionProfile
+    {
+        #region Constructors
+
+        public AbsolutePositionProfile(int posX, int posY)
+        {
+            this.PosX = posX;
+            this.PosY = posY;
+        }
+
+        #endregion
+
+        #region Properties
+
+        public int PosX
+        {
+            get;
+            private set;
+        }
+
+        public int PosY
+        {
+            get;
+            private set;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public Vector2 GetPosition(Rectangle bounds, Rectangle parentBounds)
+        {
+            return new Vector2(PosX, PosY);
+        }
+
+        #endregion
+    }
+}
