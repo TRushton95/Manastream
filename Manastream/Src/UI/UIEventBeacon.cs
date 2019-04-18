5b3e02f55b70573c827d20a5b102b35a92616bca
Tom Rushton <tom.rushton@hotmail.co.uk>
2019-04-18 23:43:59 +0100

    Add ui event beacon class

---
+++ Manastream/Src/UI/UIEventBeacon.cs
@@ -0,0 +1,44 @@
+﻿namespace Manastream.Src.UI
+{
+    #region Usings
+
+    using Manastream.Src.EventSystem;
+    using Manastream.Src.EventSystem.Events.Game;
+
+    #endregion
+
+    public class UIEventBeacon : Listener
+    {
+        #region Fields
+
+        private UIEventBeacon instance;
+
+        #endregion
+
+        #region Properties
+
+        public UIEventBeacon Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new UIEventBeacon();
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void SendEndTurnEvent()
+        {
+            eventManager.Notify(new EndTurnEvent());
+        }
+
+        #endregion
+    }
+}
